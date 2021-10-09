025dc43408301d43e60cd48f1fc4a4d69e600c6d
Harun Doğdu <harundogdu06@gmail.com>
2021-10-09 21:52:59 +0300

    provider added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ThemeProvider } from 'styled-components';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 /* pages */
 import FavouriteView from './views/favourite';
 import HistoryView from './views/history';
@@ -29,16 +30,18 @@ const SearchStack = () => {
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName="SearchStack"
-          screenOptions={{ headerShown: false }}
-          tabBar={props => <TabBar {...props} />}>
-          <Tab.Screen name="History" component={HistoryView} />
-          <Tab.Screen name="SearchStack" component={SearchStack} />
-          <Tab.Screen name="Favourite" component={FavouriteView} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            initialRouteName="SearchStack"
+            screenOptions={{ headerShown: false }}
+            tabBar={props => <TabBar {...props} />}>
+            <Tab.Screen name="History" component={HistoryView} />
+            <Tab.Screen name="SearchStack" component={SearchStack} />
+            <Tab.Screen name="Favourite" component={FavouriteView} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </ThemeProvider>
   );
 };
