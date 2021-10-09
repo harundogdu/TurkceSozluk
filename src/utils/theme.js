c1aaca96fc7345000bf8874ec40daae0a0313f2e
Harun Doğdu <harundogdu06@gmail.com>
2021-10-09 15:58:25 +0300

    react-native-svg and Logo added

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,8 +1,20 @@
 const space = [];
+const sizes = {
+  actionButton: 48,
+};
 const colors = {
   red: '#E11E3C',
-  gray: '#758291',
-  white: '#FFFFFF',
+  softRed: '#F8F8F8',
+  light: '#E8EAED',
+  birlesikKelimeDark: '#2E494C',
+  birlesikKelimeMedium: '#73A5AA',
+  birlesikKelimeLight: '#E8F0F1',
+  atasozleriDark: '#4F3822',
+  atasozleriMedium: '#BB8E62',
+  atasozleriLight: '#F9F5F1',
+  textDark: '#0A151F',
+  textMedium: '#48515B',
+  textLight: '#758291',
 };
 const radii = {
   normal: 6,
@@ -12,4 +24,5 @@ export default {
   space,
   radii,
   colors,
+  sizes,
 };
--- src/views/search/SearchView.js
+++ src/views/search/SearchView.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import {Text, Button} from 'react-native';
-import Box from '../../components/Box/BoxDefault';
+import { Button } from 'react-native';
 import BoxCenter from '../../components/Box/BoxCenter';
 /* Search Function */
-const SearchView = ({navigation}) => {
+const SearchView = ({ navigation }) => {
   return (
     <BoxCenter>
-      <Text>SearchView</Text>
       <Button title="Go Detail" onPress={() => navigation.navigate('Detail')} />
-      <Box size={20} bg="blue" mt={20} />
     </BoxCenter>
   );
 };
