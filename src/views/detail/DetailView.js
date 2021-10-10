9a7ab555a86e6f036156119068956ce7d7d2d7dd
Harun Doğdu <harundogdu06@gmail.com>
2021-10-10 17:46:27 +0300

    DetailView designed

--- src/views/detail/DetailView.js
+++ src/views/detail/DetailView.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { FlatList } from 'react-native';
 /* components */
 import Box from '../../components/Box/BoxDefault';
-import Text from '../../components/Text';
+import ListHeaderComponent from '../../components/ListHeaderComponent';
 import FocusAwareStatusBar from '../../utils/FocusAwareStatusBar';
-
+/* styles */
+import styles from './DetailView.style';
+/* DATA */
+import DATA from '../../@fake_db/db.json';
+import DetailRenderItem from '../../components/DetailRenderItem';
 const DetailView = () => {
   return (
-    <Box bg="softRed" flex={1}>
-      <FocusAwareStatusBar barStyle="dark-content" backgroundColor="white" />
-      <Text>DetailView</Text>
+    <Box bg="softRed" flex={1} px={16}>
+      <FocusAwareStatusBar barStyle="light-content" backgroundColor="red" />
+      {/* Content */}
+      <Box flex={1}>
+        <FlatList
+          ListHeaderComponent={ListHeaderComponent}
+          style={styles.bg}
+          data={DATA}
+          keyExtractor={item => item.id}
+          ItemSeparatorComponent={() => <Box height={1} bg="light" />}
+          renderItem={({ item, index }) => DetailRenderItem(item, index)}
+        />
+      </Box>
     </Box>
   );
 };
